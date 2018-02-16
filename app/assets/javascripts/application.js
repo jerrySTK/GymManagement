fa92b7c72cf32ab3f6f66f6e2a7552214ce775a3
Gerardo Romero <gromero@sitek.mx>
2018-02-16 12:23:09 -0700

    bootstap integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require javascripts/jquery
 //= require rails-ujs
 //= require turbolinks
-//= require bootstrap
+//= require javascripts/bootstrap
 //= require_tree .
